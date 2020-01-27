b6a3f254303bf7330b65df9291f7d3e099172ece
Carlos Eustáquio <caeu227@gmail.com>
2020-01-27 00:27:09 -0300

    Cria rotas de login, forget-pass e recovery-pass

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
+// components
+import Login from './pages/login'
+import ForgetPass from './pages/forget-pass'
+import RecoveryPass from './pages/recovery-pass'
 
 const Routes = () => (
   <Switch>
+    <Redirect exact from="/" to="/login" />
     <Route exact path="/" component={() => <h1>Home</h1>} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/forget-pass" component={ForgetPass} />
+    <Route exact path="/recovery-pass" component={RecoveryPass} />
   </Switch>
 )
 
